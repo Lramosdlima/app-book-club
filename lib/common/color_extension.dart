f2e7a2d3048bd50e5b531d36e528c246d5d10fdf
Leonardo <109601810+Lramosdlima@users.noreply.github.com>
2023-10-27 19:12:16 -0300

    style: :lipstick: style file

---
+++ lib/common/color_extension.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TColor {
+  static Color get primary => const Color(0xFF6C7ACC);
+  static Color get secondary => const Color(0xFF3749AF);
+  static Color get primaryLight => const Color(0xFF6670AA);
+  static Color get text => const Color(0xFF212121);
+  static Color get subTitle => const Color(0xFF212121).withOpacity(0.4);
+
+  static List<Color> get button => const [Color(0xFF3F51B5), Color(0xFF3245AF)];
+}
