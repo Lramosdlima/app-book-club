91bbfcc739b6b4128200c4f5d4b7fb4aba43c2d1
Leonardo <109601810+Lramosdlima@users.noreply.github.com>
2023-12-01 04:17:33 -0300

    feat: :sparkles: icon parameter for button component

--- lib/common/button.dart
+++ lib/common/button.dart
@@ -1,4 +1,5 @@
 import 'package:bookclub/common/color_extension.dart';
+import 'package:bookclub/common/text.dart';
 import 'package:flutter/material.dart';
 
 class AppButton extends StatelessWidget {
@@ -6,30 +7,35 @@ class AppButton extends StatelessWidget {
   final VoidCallback onPressed;
   final Color? backgroundColor;
   final Color? textColor;
+  final Icon? icon;
 
   const AppButton(
       {super.key,
       required this.text,
       required this.onPressed,
       this.backgroundColor,
-      this.textColor});
+      this.textColor,
+      this.icon});
 
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
         onPressed: onPressed,
-        textColor: textColor ?? TColor.primarytext,
+        textColor: textColor ?? TColor.primaryText,
         color: backgroundColor ?? TColor.primary,
         minWidth: double.maxFinite,
         height: 50,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        child: Text(
-          text,
-          style: TextStyle(
-            color: textColor ?? TColor.primarytextWhite,
-            fontWeight: FontWeight.w500,
-            fontSize: 17,
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            icon ?? const SizedBox(),
+            icon != null ? const SizedBox(width: 20) : const SizedBox(),
+            AppText(
+                text: text,
+                textColor: textColor ?? TColor.primaryTextWhite,
+                isTitle: true)
+          ],
         ));
   }
 }
