3243d541070483cfc5a8d3502cb2c11e424f91ff
Leonardo <109601810+Lramosdlima@users.noreply.github.com>
2023-12-02 13:13:05 -0300

    refactor: :recycle: refactor profile page

--- lib/view/profile/profile.dart
+++ lib/view/profile/profile.dart
@@ -1,3 +1,5 @@
+import 'package:bookclub/common/color_extension.dart';
+import 'package:bookclub/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -21,34 +23,70 @@ class _ProfilePageState extends State<ProfilePage> {
           ],
         ),
       ),
-      body: ListView(
-        children: const [
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Nome'),
-            subtitle: Text('John Doe'),
-          ),
-          ListTile(
-            leading: Icon(Icons.email),
-            title: Text('Email'),
-            subtitle: Text('johndoe31@gmail.com'),
-          ),
-          ListTile(
-            leading: Icon(Icons.phone),
-            title: Text('Telefone'),
-            subtitle: Text('(11) 99999-9999'),
-          ),
-          ListTile(
-            leading: Icon(Icons.location_on),
-            title: Text('Endereço'),
-            subtitle: Text('Rua dos Bobos, 0'),
-          ),
-          ListTile(
-            leading: Icon(Icons.calendar_today),
-            title: Text('Birth Date'),
-            subtitle: Text('01/01/2000'),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          children: [
+            const SizedBox(height: 10),
+            Text(
+              "Dados",
+              style: TextStyle(
+                color: TColor.primaryTextWhite,
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Divider(),
+            const ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Nome'),
+              subtitle: Text('John Doe'),
+            ),
+            const ListTile(
+              leading: Icon(Icons.email),
+              title: Text('Email'),
+              subtitle: Text('johndoe31@gmail.com'),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "Configurações",
+              style: TextStyle(
+                color: TColor.primaryTextWhite,
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.lock),
+              title: const Text('Alterar Senha'),
+              onTap: () {
+                // Navigator.pushNamed(context, AppRoutes.CHANGE_PASSWORD);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.edit),
+              title: const Text('Editar Perfil'),
+              onTap: () {
+                // Navigator.pushNamed(context, AppRoutes.EDIT_PROFILE);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.delete),
+              title: const Text('Excluir Conta'),
+              onTap: () {
+                // Navigator.pushNamed(context, AppRoutes.DELETE_ACCOUNT);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text('Sair'),
+              onTap: () {
+                Navigator.pushReplacementNamed(context, AppRoutes.WELCOME);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
