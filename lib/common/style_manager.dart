fcfe4121fd8c47e7a99afea85c0288cd984efb27
Leonardo <109601810+Lramosdlima@users.noreply.github.com>
2023-12-03 00:29:15 -0300

    refactor: :recycle: add const and remove unused color

--- lib/common/style_manager.dart
+++ lib/common/style_manager.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class StyleManager {
   String fontFamily = 'Roboto';
   String secondaryFontFamily = 'Montserrat';
-  Color primary = const Color.fromARGB(255, 204, 170, 108);
+  Color primary = Colors.amber;
   Color secondary = const Color.fromARGB(255, 175, 137, 55);
   Color primaryLight = const Color.fromARGB(255, 170, 143, 102);
   Color primaryText = const Color(0xFF212121);
@@ -11,8 +11,6 @@ class StyleManager {
   Color secondaryText = const Color(0xFF9E9E9E);
   Color backgroundColor = Colors.white;
   Color darkBackgroundColor = const Color(0xFF212121);
-
-  static List<Color> get button => const [Color(0xFF3F51B5), Color(0xFF3245AF)];
 
   static final StyleManager _singleton = StyleManager._internal();
   StyleManager._internal();
