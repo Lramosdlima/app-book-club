4a2d3819191a910913ee2a655ff4f82a3a02b389
Leonardo <109601810+Lramosdlima@users.noreply.github.com>
2023-12-03 00:49:46 -0300

    style: :art: padding style

--- lib/view/onboarding/welcome.dart
+++ lib/view/onboarding/welcome.dart
@@ -23,57 +23,59 @@ class _WelcomePageState extends State<WelcomePage> {
         ),
         SafeArea(
           child: SingleChildScrollView(
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              padding: const EdgeInsets.only(left: 150, right: 150, top: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Bem vindo ao app",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: StyleManager.instance.primaryTextWhite,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 30,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Bem vindo ao app",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: StyleManager.instance.primaryTextWhite,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 30,
+                      ),
                     ),
-                  ),
-                  Text(
-                    "Book Club!",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: StyleManager.instance.primaryTextWhite,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 30,
+                    Text(
+                      "Book Club!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: StyleManager.instance.primaryTextWhite,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 30,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: MediaQuery.of(context).size.width * 0.02),
-                  _logo(),
-                  SizedBox(height: MediaQuery.of(context).size.width * 0.11),
-                  AppButton(
-                      text: "Entrar",
-                      onPressed: () {
-                        Navigator.pushNamed(context, AppRoutes.LOGIN);
-                      }),
-                  SizedBox(height: MediaQuery.of(context).size.width * 0.06),
-                  AppButton(
-                      text: "Cadastrar",
-                      onPressed: () {
-                        Navigator.pushNamed(context, AppRoutes.SIGNUP);
-                      }),
-                  SizedBox(height: MediaQuery.of(context).size.width * 0.08),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(context, AppRoutes.HOME);
-                      },
-                      child: Text(
-                        "Acessar sem login",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: StyleManager.instance.primaryTextWhite),
-                      )),
-                ],
+                    SizedBox(height: MediaQuery.of(context).size.width * 0.02),
+                    _logo(),
+                    SizedBox(height: MediaQuery.of(context).size.width * 0.11),
+                    AppButton(
+                        text: "Entrar",
+                        onPressed: () {
+                          Navigator.pushNamed(context, AppRoutes.LOGIN);
+                        }),
+                    SizedBox(height: MediaQuery.of(context).size.width * 0.06),
+                    AppButton(
+                        text: "Cadastrar",
+                        onPressed: () {
+                          Navigator.pushNamed(context, AppRoutes.SIGNUP);
+                        }),
+                    SizedBox(height: MediaQuery.of(context).size.width * 0.08),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, AppRoutes.HOME);
+                        },
+                        child: Text(
+                          "Acessar sem login",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: StyleManager.instance.primaryTextWhite),
+                        )),
+                  ],
+                ),
               ),
             ),
           ),
