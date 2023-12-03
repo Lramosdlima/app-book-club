e81347c4307504a30a922cf4f1aeecd31d9930cd
Leonardo <109601810+Lramosdlima@users.noreply.github.com>
2023-12-03 02:18:37 -0300

    feat: :sparkles: add user store

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,9 +1,11 @@
 class User {
+  int? id;
   String? name;
   String? email;
   String? password;
 
   User({
+    this.id,
     this.name,
     this.email,
     this.password,
@@ -11,6 +13,7 @@ class User {
 
   factory User.fromMap(Map<String, dynamic> map) {
     return User(
+      id: map["id"] as int?,
       name: map["name"] as String?,
       email: map["email"] as String?,
       password: map["password"] as String?,
---
+++ lib/store/user.dart
@@ -0,0 +1,23 @@
+import 'package:mobx/mobx.dart';
+import 'package:bookclub/model/user.dart';
+part 'user.g.dart';
+
+class UserStore extends _UserStore with _$UserStore {
+  static final UserStore _instance = UserStore._internal();
+
+  factory UserStore() {
+    return _instance;
+  }
+
+  UserStore._internal();
+}
+
+abstract class _UserStore with Store {
+  @observable
+  User user = User();
+
+  @action
+  setUserData(data) {
+    user = User.fromMap(data);
+  }
+}
