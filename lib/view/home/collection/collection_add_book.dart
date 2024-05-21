1c6320644a8c0ff693852f6ab9903bd86419e689
Rafael <rafael.caneca2001@gmail.com>
2024-05-21 05:58:07 -0300

    feat:

--- lib/view/home/collection/collection_add_book.dart
+++ lib/view/home/collection/collection_add_book.dart
@@ -1,6 +1,11 @@
+import 'package:bookclub/common/book_card.dart';
+import 'package:bookclub/common/button.dart';
 import 'package:bookclub/common/loader.dart';
-import 'package:bookclub/common/style_manager.dart';
 import 'package:bookclub/model/book.dart';
+import 'package:bookclub/repository/book.dart';
+import 'package:bookclub/view/home/collection/my_collections.dart';
+import 'package:bookclub/view/home/newhome/book_detail.dart';
+import 'package:bookclub/view/home/newhome/data.dart';
 import 'package:flutter/material.dart';
 
 class CollectionAddBook extends StatefulWidget {
@@ -13,19 +18,74 @@ class CollectionAddBook extends StatefulWidget {
 class _CollectionAddBookState extends State<CollectionAddBook> {
   bool _isLoading = false;
   Loader loader = Loader();
+  late List<Book> books = [];
 
-  late List<Book> _books = [];
+  List<Book> foundedBooks = [];
 
-  List<Book> _foundedBooks = [];
+  Map<int, bool> selectedFlag = {};
+  bool isSelectionMode = false;
+  
+  List<NavigationItem> navigationItems = getNavigationItemList();
+  NavigationItem? selectedItem;
+
+  
+      void initState() {
+    super.initState();
+    setState(() {
+      // selectedFilter = filters[0];
+      selectedItem = navigationItems[0];
+    });
+    Future.delayed(Duration.zero, () {
+      _getBooks();
+    });
+  }
 
   onSearch(String search) {
     setState(() {
-      _foundedBooks = _books
+      foundedBooks = books
           .where(
               (book) => book.title!.toLowerCase().contains(search))
           .toList();
     });
   }
+
+  void onLongPress(bool isSelected, int index) {
+    setState(() {
+      selectedFlag[index] = !isSelected;
+      // If there will be any true in the selectionFlag then 
+      // selection Mode will be true
+      isSelectionMode = selectedFlag.containsValue(true);
+    });
+  }
+
+  
+  void onTap(bool isSelected, int index) {
+    if (isSelectionMode) {
+      setState(() {
+        selectedFlag[index] = !isSelected;
+        isSelectionMode = selectedFlag.containsValue(true);
+      });
+    } else {
+       Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => BookDetail(book: books[index])),
+                    );
+    }
+  }
+
+ _buildSelectIcon(bool isSelected, Book books) {
+    if (isSelectionMode) {
+      return Icon(
+        isSelected ? Icons.check_box : Icons.check_box_outline_blank,
+        color: Theme.of(context).primaryColor,
+      );
+    } else {
+   
+    }
+  }
+   
+    
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +95,7 @@ class _CollectionAddBookState extends State<CollectionAddBook> {
         title: SizedBox(
           height: 38,
           child: TextField(
-            //onChanged: (value) => onSearch(value),
+            onChanged: (value) => onSearch(value),
             style: const TextStyle(color: Colors.white),
             decoration: InputDecoration(
                 filled: true,
@@ -53,21 +113,82 @@ class _CollectionAddBookState extends State<CollectionAddBook> {
           ),
         ),
       ),
-      body: Container(
+      body: ListView.builder(
+          itemCount: books.length,
+          itemBuilder: (BuildContext context, int index) {
+          selectedFlag[index] = selectedFlag[index] ?? false;  
+          bool? isSelected = selectedFlag[index];
+            return Column(
+              children: [
+                const SizedBox(height: 15),
+                 ListTile(
+                   onLongPress: () => onLongPress(isSelected, index),
+                   onTap: () => onTap(isSelected, index),
+                  title: Text(books[index].title ?? ''),
+                  subtitle: Text(books[index].author?.name ?? ''),
+                  leading: _buildSelectIcon(isSelected!, Book()), 
+                  tileColor: Colors.grey.shade800,
+                 
+                ),
+                const SizedBox(height: 15),
+              ],
+            );
+          
+          },
+          
+        )
+        
+      /*body: Container(
           color: Colors.grey.shade900,
           child: _isLoading
               ? Loader().pageLoading()
-              : _foundedMyCollections.isNotEmpty
+               : 
                : const Center(child: Text("Nenhuma livro foi adicionado ainda"))),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // adiciona os livros a coleção
-        },
-        backgroundColor: StyleManager.instance.primary,
-        child: Icon(Icons.add),
-      ),
-      );
+               
+               */    
+                     
+    );
+
+    Widget Button() {
+      return Column (children: [
+             AppButton(
+          text: "Criar Coleção",
+          onPressed: () {
+            _goToMyCollections() ;
+          },
+            )
+      ],);
+    }
+ 
     
-    
+  }
+   List<Widget> buildBooks() {
+    List<Widget> list = [];
+    for (var i = 0; i < books.length; i++) {
+      list.add(BookCard(book: books[i]));
+    }
+    return list;
+  }
+
+    _getBooks() async {
+    setState(() => _isLoading = true);
+    final response = await BookRepository().getBooks();
+
+    if (response.status == true) {
+      setState(() {
+        books = List<Book>.from(response.data);
+      });
+    } else {
+      // ignore: avoid_print
+      print(response.error);
+    }
+    setState(() => _isLoading = false);
+  }
+
+  _goToMyCollections() {
+    Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const MyCollectionPage()),
+          );
   }
 }
