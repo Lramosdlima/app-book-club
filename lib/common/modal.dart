fd03cefe5a79a29591b6820567fe825435ff49ba
Lramosdlima <lramoslima97@gmail.com>
2024-06-11 20:16:47 -0300

    feat: :sparkles: add new dialog component

--- lib/common/modal.dart
+++ lib/common/modal.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ndialog/ndialog.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 
 typedef OnDismissCallback = void Function();
 
@@ -20,48 +21,99 @@ class Modal {
       this.message,
       this.dismissable = false});
 
-  NAlertDialog setAlert(BuildContext context) {
-    return NAlertDialog(
-      onDismiss: onDismiss,
-      backgroundColor: Colors.black.withOpacity(.80),
-      blur: 2,
-      dismissable: dismissable,
-      dialogStyle: DialogStyle(
-          borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-          elevation: 10.0),
-      actions: <Widget>[
-        TextButton(
-          child: const Text("OK"),
-          onPressed: () {
-            
-            if (onDismiss != null) {
-              onDismiss!();
-            }
-          },
-        )
-      ],
-      title: Text(title ?? '',
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-              color: Colors.white,
-              fontFamily: 'Manrope',
-              fontWeight: FontWeight.w700,
-              fontSize: 18)),
-      content: Text(message ?? '',
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-              color: Colors.white, fontFamily: 'Manrope', fontSize: 12)),
-    );
+  // NAlertDialog setAlert(BuildContext context) {
+  //   return NAlertDialog(
+  //     onDismiss: onDismiss,
+  //     backgroundColor: Colors.black.withOpacity(.80),
+  //     blur: 2,
+  //     dismissable: dismissable,
+  //     dialogStyle: DialogStyle(
+  //         borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+  //         elevation: 10.0),
+  //     actions: <Widget>[
+  //       TextButton(
+  //         child: const Text("OK"),
+  //         onPressed: () {
+
+  //           if (onDismiss != null) {
+  //             onDismiss!();
+  //           }
+  //         },
+  //       )
+  //     ],
+  //     title: Text(title ?? '',
+  //         textAlign: TextAlign.center,
+  //         style: const TextStyle(
+  //             color: Colors.white,
+  //             fontFamily: 'Manrope',
+  //             fontWeight: FontWeight.w700,
+  //             fontSize: 18)),
+  //     content: Text(message ?? '',
+  //         textAlign: TextAlign.center,
+  //         style: const TextStyle(
+  //             color: Colors.white, fontFamily: 'Manrope', fontSize: 12)),
+  //   );
+  // }
+
+  setAlert(BuildContext context) {
+    return AwesomeDialog(
+      context: context,
+      dialogType: DialogType.infoReverse,
+      headerAnimationLoop: true,
+      animType: AnimType.bottomSlide,
+      title: title,
+      reverseBtnOrder: true,
+      btnOkOnPress: () {},
+      btnCancelOnPress: () {},
+      desc: message,
+    ).show();
   }
 
-  Future<void> errorAlert(String? error, BuildContext context) async {
-    var modal = Modal(title: 'Ops...', message: '$error!').setAlert(context);
-    modal.show(context);
+  errorAlert(String? error, BuildContext context) {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.error,
+      animType: AnimType.rightSlide,
+      headerAnimationLoop: false,
+      title: 'Error',
+      desc: '$error!',
+      btnOkOnPress: () {},
+      btnOkIcon: Icons.cancel,
+      btnOkColor: Colors.red,
+    ).show();
   }
 
-  Future<void> successAlert(String? data, BuildContext context) async {
+  alertAlert(String? error, BuildContext context) {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.warning,
+      animType: AnimType.rightSlide,
+      headerAnimationLoop: false,
+      title: 'Alerta',
+      desc: '$error!',
+      btnOkOnPress: () {},
+      btnOkIcon: Icons.cancel,
+      btnOkColor: Colors.orange,
+    ).show();
+  }
+
+  successAlert(String? data, BuildContext context) {
     var message = data ?? 'Realizado com sucesso!';
-    var modal = Modal(title: 'Pronto!', message: '$message!').setAlert(context);
-    modal.show(context);
+    AwesomeDialog(
+      context: context,
+      animType: AnimType.leftSlide,
+      headerAnimationLoop: false,
+      dialogType: DialogType.success,
+      showCloseIcon: true,
+      title: 'Sucesso!',
+      desc: message,
+      btnOkOnPress: () {
+        debugPrint('OnClcik');
+      },
+      btnOkIcon: Icons.check_circle,
+      onDismissCallback: (type) {
+        debugPrint('Dialog Dissmiss from callback $type');
+      },
+    ).show();
   }
 }
