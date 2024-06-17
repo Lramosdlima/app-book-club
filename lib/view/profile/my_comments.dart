75a8a90d78d8b6cda9604aed83aeb34b5af21529
Lramosdlima <lramoslima97@gmail.com>
2024-06-16 23:05:25 -0300

    refactor: :recycle: add endpoint, loading and empty page

--- lib/view/profile/my_comments.dart
+++ lib/view/profile/my_comments.dart
@@ -1,11 +1,13 @@
+import 'package:bookclub/common/empty_page.dart';
+import 'package:bookclub/common/loader.dart';
 import 'package:bookclub/common/style_manager.dart';
 import 'package:bookclub/model/user_book_rate.dart';
+import 'package:bookclub/repository/rate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:readmore/readmore.dart';
 
 class MyCommentsPage extends StatefulWidget {
-
   const MyCommentsPage({super.key});
 
   @override
@@ -13,26 +15,55 @@ class MyCommentsPage extends StatefulWidget {
 }
 
 class _MyCommentsPageState extends State<MyCommentsPage> {
-
-  late List<UserBookRate> comments;
+  bool _isLoading = false;
+  Loader loader = Loader();
+  List<UserBookRate> _comments = [];
 
   @override
   void initState() {
-    // CHAMADA ENDPOINT
+    _fetchComments();
     super.initState();
+  }
+
+  Future<void> _fetchComments() async {
+    setState(() => _isLoading = true);
+    final response = await RateRepository().getRateByUserId();
+
+    if (response.status == true) {
+      setState(() {
+        _comments = List<UserBookRate>.from(response.data);
+      });
+    } else {
+      // ignore: avoid_print
+      print(response.error);
+    }
+    setState(() => _isLoading = false);
   }
 
   @override
   Widget build(BuildContext context) {
-
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: comments.length,
-      itemBuilder: (BuildContext context, int index) {
-        return buildComment(comments[index]);
-      },
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Minhas Avaliações',
+            style: TextStyle(color: StyleManager.instance.primary),
+          ),
+        ),
+        body: Container(
+          child: _isLoading
+              ? Loader().pageLoading()
+              : _comments.isNotEmpty
+                  ? ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: _comments.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return buildComment(_comments[index]);
+                      },
+                    )
+                  : const EmptyPage(
+                      text: "Nenhuma avaliação sua foi encontrada..."),
+        ));
   }
 
   Widget buildComment(UserBookRate comment) {
@@ -45,8 +76,12 @@ class _MyCommentsPageState extends State<MyCommentsPage> {
               Row(
                 children: [
                   comment.user?.profile_picture != null
-                  ? CircleAvatar(backgroundImage:  NetworkImage(comment.user?.profile_picture ?? ''))
-                  : const CircleAvatar(child: Icon(Icons.person, color: Colors.white, size: 30)),
+                      ? CircleAvatar(
+                          backgroundImage:
+                              NetworkImage(comment.user?.profile_picture ?? ''))
+                      : const CircleAvatar(
+                          child: Icon(Icons.person,
+                              color: Colors.white, size: 30)),
                   const SizedBox(width: 16.0),
                   Text(comment.user?.name ?? ''),
                 ],
